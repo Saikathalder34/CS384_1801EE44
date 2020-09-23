8094a1be839b903303102b8cc2168c6aa0cd1a03
saikathalder34 <saikathalder301@gmail.com>
2020-09-23 19:51:40 +0530

    MSE function added to the program

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -15,3 +15,20 @@ def mean(first_list):
     mean_value = summation(first_list)/len(first_list)
     mean_value = round(mean_value, 3)
     return mean_value
+
+
+# Function to compute mse. You cant use Python functions
+def mse(first_list, second_list):
+    list_dummy = []
+    if len(first_list) == len(second_list):
+        for x, y in zip(first_list, second_list):
+            if type(x) == type("s") or type(y) == type("s"):
+                return 0
+            else:
+                list_dummy.append(x*x-y*y)
+
+    else:
+        return 0
+    mse_value = summation(list_dummy)/len(list_dummy)
+    mse_value = round(mse_value, 3)
+    return mse_value
